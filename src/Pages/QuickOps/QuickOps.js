cdaaf9fc8fd15a453898b4e8886846e9a6ec7bd4
Daniel Pires <danielpires6@hotmail.com>
2020-10-31 11:58:24 -0300

    feature: Criaçãode do header

--- src/Pages/QuickOps/QuickOps.js
+++ src/Pages/QuickOps/QuickOps.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import './QuickOps.scss';
+
 const QuickOps = () => {
-    return (
-        <h1>QuickOps</h1>
-    );
+  return (
+    <header className='header'>
+      <strong className='header__title'> Reembolso - Confraternização </strong>
+      <div className='header__space_around'>
+        <ul className='margin'>
+          <li className='header__item'>
+            <strong className='header__label' > Justificativa: </strong>
+            <p className='header__paragraph' > Reembolso referente a confraternização das equipes Backoffice / BI / Analytics.</p>
+          </li>
+          <li className='header__item'>
+            <strong className='header__label' > Finalidade: </strong>
+            <p className='header__paragraph' > Confraternização</p>
+          </li>
+          <li className='header__item'>
+            <strong className='header__label' > Quantidade: </strong>
+            <p className='header__paragraph' > 33 pessoas</p>
+          </li>
+          <li className='header__item'>
+            <strong className='header__label'> Projeto: </strong>
+            <p className='header__paragraph'> - </p>
+          </li>
+        </ul>
+        <div className='header__flex'>
+            <hr/>
+            <div className='header__grid'>
+            <ul >
+              <li>
+                <strong className='header__label' > Centro de Custo: </strong>
+              </li>
+              <li className='header__item'>
+                <p className='header__paragraph'> 50% - TEC-DEV-Back Office </p>
+              </li>
+              <li className='header__item'>
+                <p className='header__paragraph'> 50% - BKO-DEV-BI-Analytics </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='header__flex'>
+            <hr/>
+            <div className='header__grid'>
+            <ul className='header__resume'>
+              <li className='header__item'>
+                <p className='header__paragraph'> Esta solicitação será paga com o Budget de educação / confraternização. </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default QuickOps;
