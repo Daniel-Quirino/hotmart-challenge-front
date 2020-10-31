c1092c02d0515aa3feed4b0f262ddbbadf6b5517
Daniel Pires <danielpires6@hotmail.com>
2020-10-31 19:38:39 -0300

    feature: Adicionado primeiro layout da timeline

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import QuickOps from './Pages/QuickOps';
+import Timeline from './Pages/Timeline';
 
 
 export const Routes = () => {
   return (
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact={true} component={QuickOps} />
+            <Route path="/" exact={true} component={Timeline} />
         </Switch>
     </BrowserRouter>  
   );
