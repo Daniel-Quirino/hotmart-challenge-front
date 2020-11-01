14bb3427005ff2cdc21c5ba0704d4b51676cfaa7
Daniel Pires <danielpires6@hotmail.com>
2020-11-01 17:40:29 -0300

    test: Created unit tests for a function to format the header title

--- src/Pages/Header/utils.js
+++ src/Pages/Header/utils.js
@@ -1,15 +1,9 @@
-import headerTypes from './Enum/HeaderTypes';
+import headerTypes from '../../Enum/HeaderTypes';
 
 export const handleHeaderTitle = (header) => {
-  if(!header || !header.type) return 'Reembolso ou Adiantamento...';
+  if(!header || !header.type || !header.id) return 'Reembolso ou Adiantamento...';
   const { type, id } = header;
   const SPACING = ' - ';
-
-  console.log(headerTypes);
-
-  console.log(header)
-  console.log(type);
-  console.log(headerTypes[type])
 
   return headerTypes[type]
           .concat(SPACING)
