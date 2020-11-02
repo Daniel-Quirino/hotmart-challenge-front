7ed7f6dc9b4a09b6d2805d871d8fd38dbc2c92af
Daniel Pires <danielpires6@hotmail.com>
2020-11-01 23:18:11 -0300

    feat: Finished header MVP

---
+++ src/Pages/AnticipationRefund/Components/Header/Header.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+
+import { handleHeaderTitle, getPurposeByIdentifier } from './utils';
+
+import { Store } from '../../../../Infrastructure/Store/Store';
+import doGetHeader from '../../../../Infrastructure/Actions/Header';
+
+import './Header.scss';
+
+const Header = () => {
+  const { state, dispatch } = React.useContext(Store);
+  const [ header, setHeader ] = React.useState();
+
+  useEffect(() => {
+    doGetHeader(dispatch);
+  }, []);
+
+  useEffect(() => {
+    if(state.header) setHeader(state.header);
+  }, [state]);
+
+  const renderHeaderResumeList = () => {
+    return (
+      <ul className='header__resume'>
+        {renderHeaderResumeItem('Justificativa', header.justification)}
+        {renderHeaderResumeItem('Finalidade', getPurposeByIdentifier(header.purpose))}
+        {renderHeaderResumeItem('Quantidade', header.accountabilityExtraInfo.amountOfPeople)}
+        {renderHeaderResumeItem('Projeto', header.project.title)}
+      </ul>
+    )
+  }
+
+  const renderHeaderResumeItem = (label, paragraph) => {
+    return(
+      <li className='header__item'>
+        <strong className='header__label' > {label}: </strong>
+        <p className='header__paragraph' > {paragraph} </p>
+      </li>
+    )
+  }
+
+  const renderCoste = (costCenters) => {
+    return (
+      <div className='header__coste'>
+        <ul >
+          <li>
+            <strong className='header__label' > Centro de Custo: </strong>
+          </li>
+          {renderListOfCostCenters(costCenters)}
+        </ul>
+      </div>
+    )
+  }
+
+  const renderListOfCostCenters = (costCenters) => {
+    return costCenters.map(cost => {
+      return (
+        <li className='header__item'>
+          <p className='header__paragraph'> {`${cost.percentage}% - ${cost.reviser.name}`} </p>
+        </li>
+      )
+    });
+  }
+
+  const renderIsEducationBudget = (isEducation) => {
+    return (
+      <div className='header__education'>
+        <FontAwesomeIcon icon={faGraduationCap} size="2x"/>
+    <p className='header__paragraph'> Esta solicitação {isEducation ? '' : <b>não</b>} será paga com o Budget de educação / confraternização. </p>
+      </div>
+    )
+  }
+
+
+  if(!header || !header.accountabilityExtraInfo)
+    return <></>
+
+  return (
+    <header className='header__container'>
+      <div className='header__grid'>
+        <strong className='header__title'> {handleHeaderTitle(header)} </strong>
+        {renderHeaderResumeList()}
+        <div className='header__hr1'>
+          <hr/>
+        </div>
+        {renderCoste(header.costCenters)}
+        <div className='header__hr2'>
+          <hr/>
+        </div>
+        {renderIsEducationBudget(header.fraternizationEducationBudget)}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Pages/AnticipationRefund/Components/Header/utils.js
@@ -0,0 +1,39 @@
+import { enumTypes , enumPurposes } from '../../../../Enum/Header';
+
+export const handleHeaderTitle = (header) => {
+  
+  if (!header || (!header.type && !header.purpose)) {
+    return 'Reembolso ou Adiantamento...';
+  } else if (!header.type) {
+    return `Sem tipo - ${getPurposeByIdentifier(header.purpose)}`;
+  } else if (!header.purpose) {
+    return `${getTypeByIdentifier(header.type)} - Sem motivo`;
+  }
+  
+  return getTypeByIdentifier(header.type)
+          .concat(' - ')
+          .concat(getPurposeByIdentifier(header.purpose));  
+}
+
+export const getTypeByIdentifier = (type) => {
+  return enumTypes[type];
+}
+
+export const getPurposeByIdentifier = (purpose) => {
+  return enumPurposes[purpose];
+}
+
+
+export const formatAmountOfPeople = (amountOfPeople) => {
+  let formatText = ''
+
+  if (amountOfPeople === 1) {
+    formatText = '1 pessoa';
+  } else if (amountOfPeople > 1) {
+    formatText = `${amountOfPeople} pessoas`;
+  } else {
+    formatText = 'Nenhuma pessoa';
+  }
+
+  return formatText;
+}
