1a9af16f0c4a613e4698d6b54af0c85af52cb918
Daniel Pires <danielpires6@hotmail.com>
2020-11-04 10:02:25 -0300

    feat: added part of the component of adding new expenses with the part of sending receipt

--- src/Pages/Refund/Components/AddExpense/AddExpense.js
+++ src/Pages/Refund/Components/AddExpense/AddExpense.js
@@ -4,7 +4,26 @@ import './AddExpense.scss';
 
 const AddExpense = (props) => {
   return (
-    <h1> Adicionar uma nova despesa :) </h1>
+    <div className='add-expense-padding'>
+      <div className='add-expense'>
+        <div className='add-expense__title'>
+          <span className='add-expense__title__text'> Adicionar Despesa </span>
+        </div>
+        <div className='add-expense__receipt'>
+          <div className='add-expense__container'>
+            <span className='add-expense__receipt__title'>Envie o comprovante</span>
+            <span className='add-expense__receipt__tipe'>Você pode enviar arquivos nos formatos PNG, JPG ou PDF, tamanho máximo 10MB</span>
+            <span className='add-expense__receipt__choose'>Escolher arquivo</span>
+          </div>
+        </div>
+        <div className='add-expense__inputs'>
+          <span>INPUTS</span>
+        </div>
+        <div className='add-expense__buttons'>
+          <span>CANCELAR / ADICIONAR</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
