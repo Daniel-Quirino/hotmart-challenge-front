b148e5993339e002a5e67a5f1ec75f774d63481e
Daniel Pires <danielpires6@hotmail.com>
2020-11-04 07:17:49 -0300

    feat: Add button to create a new refund

--- src/Pages/Refund/Refund.js
+++ src/Pages/Refund/Refund.js
@@ -2,17 +2,28 @@ import React from 'react';
 import Header from './Components/Header';
 import Timeline from './Components/Timeline';
 import SideBar from './Components/SideBar';
+import Button from './Components/Button/Button';
+
 
 import './Refund.scss';
 
 const AnticipationRefund = () => {
+  // const { state, dispatch } = React.useContext(Store);
+  const [ addExpense, setAddExpanse ] = React.useState(false);
+
+  const addNewExpense = () => {
+    setAddExpanse(!addExpense)
+  }
+  
   return (   
       <div className='quick_ops__grid'>
         <div className='quick_ops__header-timeline'>
           <Header />
-          <Timeline />
+          <div className='quick_ops__expense-button'>
+            <Button addNewExpense={addNewExpense}/>
+          </div>
+          {!addExpense ? <Timeline /> : <></>}
         </div>
-        
         <div className='quick_ops__side-bar' >
           <SideBar  />
         </div>
